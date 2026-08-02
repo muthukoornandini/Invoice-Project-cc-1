cce213a20cf07aedfcde3f7972e72f9b314e1973
muthukoornandini <muthukoornandini@gmail.com>
2026-08-02 09:29:40 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,21 +1,47 @@
 
 
 
-const mysql = require("mysql2");
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
 
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "mysql@123",
-    database: "invoice_management"
+router.post("/login", (req, res) => {
+
+    const { username, password } = req.body;
+
+    const sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+
+    db.query(sql, [username, password], (err, result) => {
+
+        if (err) {
+
+            console.log("Login Error:", err);
+
+            return res.status(500).json({
+                success: false,
+                message: err.message
+            });
+
+        }
+
+        if (result.length > 0) {
+
+            return res.json({
+                success: true,
+                message: "Login successful"
+            });
+
+        } else {
+
+            return res.json({
+                success: false,
+                message: "Invalid username or password"
+            });
+
+        }
+
+    });
+
 });
 
-db.connect((err) => {
-    if (err) {
-        console.log("Database connection failed:", err);
-    } else {
-        console.log("MySQL Connected");
-    }
-});
-
-module.exports = db;
+module.exports = router;
